b01d5eb7d8709f48dc8323d0fe6a7fdc1553696b
mapleattendance <89829586+mapleattendance@users.noreply.github.com>
2021-09-08 18:17:16 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+var img = document.getElementById('img');
+
+img.addEventListener('click',async () => {
+  const response = await fetch(img.src);
+var blob =await response.blob();
+  const filesArray = [
+    new File(
+      [blob],
+      'meme.jpg',
+      {
+        type: "image/jpeg",
+        lastModified: new Date().getTime()
+      }
+    )
+  ];
+  const shareData = {
+    files: filesArray,
+  };
+  navigator.share(shareData);
+});
